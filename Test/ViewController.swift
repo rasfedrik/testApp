706e2f1f00cfac86b26a79d6e51b81029ed7f1a9
Semen Belyakov <kuragad@gmail.com>
2022-03-07 14:22:03 +0500

    a

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         networkManager.obtainQuestions(path: path) { (result) in
-            guard let items = result.items else { return  print("none")}
+//            guard let items = result.items else { return  print("none")}
             
 //            for i in items {
 //                print(i.tags)
